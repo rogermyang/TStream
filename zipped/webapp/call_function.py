2d0ce7313154a4824ebd1d557de1b1c07289ff6b
basilwong <wong.bas@gmail.com>
2019-01-27 10:38:54 -0800

    home page rough draft

---
+++ zipped/webapp/call_function.py
@@ -0,0 +1,5 @@
+import time
+
+def call_function(user_input):
+
+    return user_input
--- zipped/webapp/routes.py
+++ zipped/webapp/routes.py
@@ -10,14 +10,13 @@ import os
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    egg_filename = os.path.join(app.config['DINO_FOLDER'], 'egg.png')
-    background_file = os.path.join(app.config['BACKGROUND_FOLDER'], 'grassy_field.jpg')
+    egg_filename = os.path.join(app.config['DINO_FOLDER'], 'Egg.png')
     form = FeedForm()
     if form.validate_on_submit():
         print("validated")
         return redirect('/eat', form.user_input)
     print("not validated")
-    return render_template('home.html', form=form, egg_file=egg_filename, background_image=background_file)
+    return render_template('home.html', form=form, egg_file=egg_filename)
 
 @app.route('/eat')
 def eat(user_input):
