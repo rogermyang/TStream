09a65814f25cb785519dd4ab307a753ff6fb5f32
basilwong <wong.bas@gmail.com>
2019-01-26 17:35:12 -0800

    moved template folder

--- env/Scripts/Activate.ps1
+++ env/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\wongb\Documents\GitHub\python-docs-hello-world\env"
+$env:VIRTUAL_ENV="C:\Users\wongb\Documents\GitHub\TStream\env"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
