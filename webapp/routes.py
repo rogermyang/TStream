fde75e142c2a976658254320b79d22e6cc6f0d44
basilwong <wong.bas@gmail.com>
2019-01-27 11:19:42 -0800

    changed hatched picture

--- webapp/routes.py
+++ webapp/routes.py
@@ -15,16 +15,33 @@ def home():
     form = FeedForm()
     if form.validate_on_submit():
         print("validated")
-        return redirect('/eat', form.user_input)
+        return redirect('/eat')
     print("not validated")
-    return render_template('home.html', form=form, egg_file=egg_filename, background_image=background_file)
+    return render_template('home.html', form=form, dino_file=egg_filename, background_image=background_file)
 
-@app.route('/eat')
-def eat(user_input):
-    e = requests.get('https://frontsend.azurewebsites.net/').content
-    # print(e)
-    return render_template('eat.html')
+@app.route('/eat', methods=['GET', 'POST'])
+def eat():
+    # e = requests.get('https://frontsend.azurewebsites.net/').content
+    user_input = "EAT"
+    print(user_input)
+    hatchd_filename = os.path.join(app.config['DINO_FOLDER'], 'hatched.png')
+    background_file = os.path.join(app.config['BACKGROUND_FOLDER'], 'grassy_field.jpg')
+    form = FeedForm()
+    if form.validate_on_submit():
+        print("validated")
+        return redirect('/fed')
+    print("not validated")
+    return render_template('eat.html', form=form, uin=user_input, dino_file=hatchd_filename, background_image=background_file)
 
-@app.route('/fed')
+@app.route('/fed', methods=['GET', 'POST'])
 def fed():
-    return render_template('fed.html')
+    user_input = "FED"
+    print(user_input)
+    dino_filename = os.path.join(app.config['DINO_FOLDER'], 'dino.png')
+    background_file = os.path.join(app.config['BACKGROUND_FOLDER'], 'grassy_field.jpg')
+    form = FeedForm()
+    if form.validate_on_submit():
+        print("validated")
+        return redirect('/eat')
+    print("not validated")
+    return render_template('fed.html', form=form, uin=user_input, dino_file=dino_filename, background_image=background_file)
