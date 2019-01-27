9637e601d29283031a2d73a3b8861723d84514f5
lawmhs <matthew.hs.law@hotmail.com>
2019-01-26 21:32:17 -0800

    implemented the instagram login stuff in the main function

--- generator/main.py
+++ generator/main.py
@@ -1,3 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from InstagramAPI import InstagramAPI
+from PIL import Image
 import sys
 import nltk
 import textwrap
@@ -8,10 +12,10 @@ import model as md
 
 def main():
 
-
     src_path = str(sys.argv[1]) # data path
     lines = int(sys.argv[2]) # approximately how many words in text to generate
     ln_break = int(sys.argv[3]) # how many characters before newline in output file
+    photo_path = sys.argv[4]
 
     # actually run this
     #'''
@@ -40,6 +44,27 @@ def main():
         sample = md.sample_sentence(tm, lines)
         print(textwrap.fill(sample, ln_break))
         print(textwrap.fill(sample, ln_break), file=f)
+
+
+    # or you could also just post it to instagram
+
+    user = input('Please enter username: ')
+    pw = input('Password: ')
+
+    browser = webdriver.Chrome("/usr/bin/chromedriver")
+    browser.get("https://www.instagram.com/accounts/login")
+
+    userInput = browser.find_elements_by_css_selector('form input')[0]
+    pwInput = browser.find_elements_by_css_selector('form input')[1]
+
+    userInput.send_keys(user)
+    pwInput.send_keys(pw)
+    pwInput.send_keys(Keys.ENTER)
+
+    ig = InstagramAPI(user, pw)
+    ig.login()
+    ig.uploadPhoto(photo_path, caption=sample)
+
 
 if __name__ == '__main__':
     main()
