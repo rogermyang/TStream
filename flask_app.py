deb6fea2c64e461558238e79a1a9d2728b3bf7aa
basilwong <wong.bas@gmail.com>
2019-02-18 12:49:15 -0800

    removed virtual env from repository, added run function for flask on pythonanywhere

---
+++ flask_app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from webapp.config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+from webapp import routes
+
+if __name__ == '__main__':
+    app.run()
