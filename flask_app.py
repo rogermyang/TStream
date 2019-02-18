4866b5ab8711e8f43bf7d14a4274dbc9c810faef
basilwong <wong.bas@gmail.com>
2019-02-18 14:01:15 -0800

    change config

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,13 @@ from flask import Flask
 from webapp.config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config)
+# app.config.from_object(Config)
+
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+DINO_FOLDER = os.path.join('static', 'dino')
+BACKGROUND_FOLDER = os.path.join('static', 'backgrounds')
+CRED_PATH = os.path.join('static', 'creds')
+
 
 from webapp import routes
 
