342270994057db19fd5d7d77260f58146039534a
basilwong <wong.bas@gmail.com>
2019-02-18 15:54:42 -0800

    moved config file|

--- flask_app.py
+++ flask_app.py
@@ -2,13 +2,7 @@ from flask import Flask
 from webapp.config import Config
 
 app = Flask(__name__)
-# app.config.from_object(Config)
-
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-DINO_FOLDER = os.path.join('static', 'dino')
-BACKGROUND_FOLDER = os.path.join('static', 'backgrounds')
-CRED_PATH = os.path.join('static', 'creds')
-
+app.config.from_object(Config)
 
 from webapp import routes
 
